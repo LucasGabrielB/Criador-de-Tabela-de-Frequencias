5260d3ceee2821850a35f71f2eb79ed7309a0666
Lucas Gabriel <48874910+LucasGabrielB@users.noreply.github.com>
2020-03-07 22:55:27 -0300

    Adicionada interface gráfica.
    Adicionada interface gráfica.
    Alterada a forma de leitura de dados, agora os dados são lidos de um arquivo:  "dados.txt".

--- tabela_de_frequencia.py
+++ tabela_de_frequencia.py
@@ -2,40 +2,72 @@
 #
 # Author: Lucas Gabriel B
 
+import tkinter as tk
+from tkinter import ttk
 
-# colect the data and save in a sorted list 
-data = [int(i) for i in input().strip().split()]
-data.sort()
 
-# count the frequencies of each number
-# saves in dictionary
-frequencies = dict()
-for n in data:
-    if n in frequencies:
-        frequencies[n] += 1
-    else:
-        frequencies[n] = 1
-
-# write a table with the results in the console
-
-# print the header
-print(f'X\t|f\t|fa\t|fr\t|fra')
-print('-' * 40)
-
-fr = fa = fra = 0
-for value, frequency in frequencies.items():
-    total_of_data = len(data)
+def calculate_frequency_table():
+    ''' doc string '''
     
-    # calculate the relative frequency
-    fr = int(round((frequency / total_of_data), 2) * 100)
+    # clean the list box
+    frequency_table.delete(*frequency_table.get_children())
+    
+    # read the data  of the file and save in a sorted list
+    with open('dados.txt', 'r') as file:
+        data = [float(i.replace(',', '.')) for i in file.readlines()]
 
-    # calculate cumulative frequency
-    fa += frequency
+    data.sort()
 
-    # calculate cumulative relative frequency
-    fra += fr
+    # count the frequencies of each number
+    # saves in dictionary
+    frequencies = dict()
+    for n in data:
+        if n in frequencies:
+            frequencies[n] += 1
+        else:
+            frequencies[n] = 1             
 
-    # print the line with the results
-    print(f'{value}\t|{frequency}\t|{fa}\t|{fr}%\t|{fra}%')
+    fr = fa = fra = 0
+    for value, frequency in frequencies.items():
+        total_of_data = len(data)
+        
+        # calculate the relative frequency
+        fr = round((frequency / total_of_data) * 100, 2)
 
-print('-' * 40)
+        # calculate cumulative frequency
+        fa += frequency
+
+        # calculate cumulative relative frequency
+        fra += fr
+
+        # add the line with the results
+        frequency_table.insert("", "end", values=(value, frequency, f'{fr} %', fa, f'{round(fra, 2)} %'))
+
+# create the screen
+screen = tk.Tk()
+screen.title('Criador De Tabela De Frequência Simples')
+
+# create a title
+titleLabel = tk.Label(screen, text="Tabela de frequências", font=("Arial",30)).grid(row=0, columnspan=2)
+
+# create Treeview with 5 columns
+cols = ('X','Frequência Absoluta', 'Frequência Relativa %', 'Frequência Absoluta Acumulada', 'Frequência Relativa Acumulada %')
+frequency_table = ttk.Treeview(screen, columns=cols, show='headings')
+
+# set column headings
+for col in cols:
+    frequency_table.heading(col, text=col)
+
+frequency_table.grid(row=1, column=0, columnspan=2)
+
+# call the function to calculate and show the frequency_table
+calculate_frequency_table()
+
+# add a roload button
+reloadButton = tk.Button(screen, text="Recarregar", width=15, command=calculate_frequency_table).grid(row=3, column=0)
+
+#add a exit button
+closeButton = tk.Button(screen, text="Sair", width=15, command=exit).grid(row=3, column=1)
+
+# create the main loop for the screen
+screen.mainloop()
